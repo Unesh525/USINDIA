5cf5b08e9d04e974b3016180d1132fad446dcf21
Umesh Kumar Meghwal <umeshmeghwa605@gmail.com>
2025-08-06 22:39:39 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -86,20 +86,24 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
 import os
-import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
+
 
 
 
