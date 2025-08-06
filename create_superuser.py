63f72b54d6433932d3ac51440f2171c9f1036b45
Umesh Kumar Meghwal <umeshmeghwa605@gmail.com>
2025-08-07 02:01:46 +0530

    Update create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -12,6 +12,12 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+# auto_migrate.py
+
+import os
+os.system('python manage.py makemigrations')
+os.system('python manage.py migrate')
+os.system('echo "from django.contrib.auth.models import User; User.objects.create_superuser(\'admin\', \'admin@example.com\', \'adminpass\')" | python manage.py shell')
 
 if not User.objects.filter(username="admin").exists():
     User.objects.create_superuser("admin", "admin@example.com", "adminpassword")
